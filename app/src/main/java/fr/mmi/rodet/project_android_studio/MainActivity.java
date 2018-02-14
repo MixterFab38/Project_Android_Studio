f443f6d49d5a64bc9c2fbc100acfd1825dc5e408
kevinGRASSET <ke.grasset@laposte.net>
2018-02-14 16:10:06 +0100

    Working Flash Lite

--- app/src/main/java/fr/mmi/rodet/project_android_studio/MainActivity.java
+++ app/src/main/java/fr/mmi/rodet/project_android_studio/MainActivity.java
@@ -2,114 +2,172 @@ package fr.mmi.rodet.project_android_studio;
 
 
 
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
-import android.hardware.Camera;
-import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.Bundle;
 import android.app.Activity;
-
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toast;
+
 
 
 public class MainActivity extends Activity {
 
     TextView textLIGHT_available, textLIGHT_reading;
-    private Button button;
-    private TextView blanc;
-    private boolean isFlashOn = false;
-    private Camera camera;
+
+
+    private CameraManager mCameraManager;
+    private String mCameraId;
+    private ImageButton mTorchOnOffButton;
+    private Boolean isTorchOn;
+    private MediaPlayer mp;
 
 
 
-
+    @SuppressLint("WrongViewCast")
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Log.d("FlashLightActivity", "onCreate()");
+        mTorchOnOffButton = (ImageButton) findViewById(R.id.button_on_off);
+        isTorchOn = false;
+        Boolean isFlashAvailable = getApplicationContext().getPackageManager()
+                .hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
 
+        if (!isFlashAvailable) {
 
-        button = (Button) findViewById(R.id.flash);
-        blanc = (TextView) findViewById(R.id.barreEtat);
-
-        Context context = this;
-        PackageManager pm = context.getPackageManager();
-
-        if (!pm.hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
-            Toast.makeText(getApplicationContext(),"Votre téléphone ne possède pas de flash!", Toast.LENGTH_SHORT).show();
+            AlertDialog alert = new AlertDialog.Builder(MainActivity.this)
+                    .create();
+            alert.setTitle("Error !!");
+            alert.setMessage("Your device doesn't support flash light!");
+            alert.setButton(DialogInterface.BUTTON_POSITIVE, "OK",
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            // closing the application
+                            finish();
+                            System.exit(0);
+                        }
+                    });
+            alert.show();
             return;
         }
-        camera = Camera.open();
-        final Camera.Parameters p = camera.getParameters();
 
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View arg0) {
-                if (isFlashOn) {
-                    p.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
-                    camera.setParameters(p);
-                    isFlashOn = false;
-                    //on change la couleur de la zone en gris signifiant que le flash est éteint
-                    blanc.setBackgroundColor(Color.rgb(51, 51, 51));
-                } else {
-                    //on change la couleur de la zone en blanc signifiant que le flash est allumé
-                    blanc.setBackgroundColor(Color.WHITE);
-                    p.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
-                    camera.setParameters(p);
-                    isFlashOn = true;
+        mCameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try {
+            mCameraId = mCameraManager.getCameraIdList()[0];
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+
+        mTorchOnOffButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    if (isTorchOn) {
+                        turnOffFlashLight();
+                        isTorchOn = false;
+                    } else {
+                        turnOnFlashLight();
+                        isTorchOn = true;
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
             }
         });
 
 
-        textLIGHT_available
-                = (TextView)findViewById(R.id.LIGHT_available);
-        textLIGHT_reading
-                = (TextView)findViewById(R.id.LIGHT_reading);
 
-        SensorManager mySensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+
+
+        textLIGHT_available
+                = (TextView) findViewById(R.id.LIGHT_available);
+        textLIGHT_reading
+                = (TextView) findViewById(R.id.LIGHT_reading);
+
+        SensorManager mySensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 
         Sensor LightSensor = mySensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
-        if(LightSensor != null){
+        if (LightSensor != null) {
             textLIGHT_available.setText("Sensor.TYPE_LIGHT Available");
             mySensorManager.registerListener(
                     LightSensorListener,
                     LightSensor,
                     SensorManager.SENSOR_DELAY_NORMAL);
 
-        }else{
+        }
+        else
+            {
             textLIGHT_available.setText("Sensor.TYPE_LIGHT NOT Available");
         }
     }
 
-    private final SensorEventListener LightSensorListener = new SensorEventListener()
-    {
+
+
+
+    private final SensorEventListener LightSensorListener = new SensorEventListener() {
 
         @Override
-        public void onAccuracyChanged(Sensor sensor, int accuracy)
-        {
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
             // TODO Auto-generated method stub
 
         }
 
         @Override
         public void onSensorChanged(SensorEvent event) {
-            if(event.sensor.getType() == Sensor.TYPE_LIGHT){
+            if (event.sensor.getType() == Sensor.TYPE_LIGHT) {
                 textLIGHT_reading.setText("LIGHT: " + event.values[0]);
             }
         }
 
     };
 
+    public void turnOnFlashLight() {
 
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                mCameraManager.setTorchMode(mCameraId, true);
+
+                mTorchOnOffButton.setImageResource(R.drawable.ic_launcher_background);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void turnOffFlashLight() {
+
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                mCameraManager.setTorchMode(mCameraId, false);
+
+                mTorchOnOffButton.setImageResource(R.mipmap.ic_launcher);
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 
 }
+
+
+
+
