9e24dde3f1498a4c74da87059f97d10cc173b3e6
kevinGRASSET <ke.grasset@laposte.net>
2018-02-15 15:31:13 +0100

    Working Flash Lite with sensor < 160 (version lite)

--- app/src/main/java/fr/mmi/rodet/project_android_studio/MainActivity.java
+++ app/src/main/java/fr/mmi/rodet/project_android_studio/MainActivity.java
@@ -1,9 +1,7 @@
 package fr.mmi.rodet.project_android_studio;
 
 import android.annotation.SuppressLint;
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -11,27 +9,22 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraManager;
-import android.media.MediaPlayer;
 import android.os.Build;
 import android.os.Bundle;
 import android.app.Activity;
-import android.text.style.UpdateAppearance;
 import android.util.Log;
-import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class MainActivity extends Activity
 {
     // Sensor light
-    TextView textLIGHT_available, textLIGHT_reading;
+    TextView textLIGHT_reading;
 
     // flash light
     private CameraManager mCameraManager;
     private String mCameraId;
     private ImageButton mTorchOnOffButton;
-    private Boolean isTorchOn;
-    private MediaPlayer mp;
     private Float i;
 
 
@@ -46,50 +39,23 @@ public class MainActivity extends Activity
         // flash light
         Log.d("FlashLightActivity", "onCreate()");
         mTorchOnOffButton = (ImageButton) findViewById(R.id.button_on_off);
-        isTorchOn = false;
-        Boolean isFlashAvailable = getApplicationContext().getPackageManager()
-                .hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
-
-        if (!isFlashAvailable)
-        {
-
-            AlertDialog alert = new AlertDialog.Builder(MainActivity.this)
-                    .create();
-            alert.setTitle("Error !!");
-            alert.setMessage("Your device doesn't support flash light!");
-            alert.setButton(DialogInterface.BUTTON_POSITIVE, "OK",
-                    new DialogInterface.OnClickListener()
-                    {
-                        public void onClick(DialogInterface dialog, int which)
-                        {
-                            // closing the application
-                            finish();
-                            System.exit(0);
-                        }
-                    });
-            alert.show();
-            return;
-        }
-
+        Boolean isFlashAvailable = getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
         mCameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+
+        // Sensor light
+        textLIGHT_reading = (TextView) findViewById(R.id.LIGHT_reading);
+        SensorManager mySensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        Sensor LightSensor = mySensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        mySensorManager.registerListener(LightSensorListener, LightSensor, SensorManager.SENSOR_DELAY_NORMAL);
 
         try
         {
             mCameraId = mCameraManager.getCameraIdList()[0];
 
-
-
         } catch (CameraAccessException e)
         {
             e.printStackTrace();
         }
-
-
-
-
-
-
-
 
 
         Thread t = new Thread() {
@@ -104,19 +70,14 @@ public class MainActivity extends Activity
                             public void run() {
                                 try
                                 {
-
-
                                     if(i<= 160)
                                     {
                                         mCameraManager.setTorchMode(mCameraId, true);
-
                                     }
                                     else
                                     {
                                         mCameraManager.setTorchMode(mCameraId, false);
                                     }
-
-
                                 } catch (Exception e)
                                 {
                                     e.printStackTrace();
@@ -124,46 +85,14 @@ public class MainActivity extends Activity
                             }
                         });
                     }
-                } catch (InterruptedException e) {
+                }
+                catch (InterruptedException e)
+                {
                 }
             }
         };
 
         t.start();
-
-
-
-
-
-
-
-
-        // Sensor light
-        textLIGHT_available
-                = (TextView) findViewById(R.id.LIGHT_available);
-        textLIGHT_reading
-                = (TextView) findViewById(R.id.LIGHT_reading);
-        SensorManager mySensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        Sensor LightSensor = mySensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
-
-
-
-
-        if (LightSensor != null)
-        {
-
-            textLIGHT_available.setText("Sensor.TYPE_LIGHT NOT Available");
-            mySensorManager.registerListener(
-                    LightSensorListener,
-                    LightSensor,
-                    SensorManager.SENSOR_DELAY_NORMAL);
-        }
-        else
-        {
-            textLIGHT_available.setText("Sensor.TYPE_LIGHT NOT Available");
-        }
-
-
     }
 
 
@@ -171,7 +100,6 @@ public class MainActivity extends Activity
     // Sensor light
     private final SensorEventListener LightSensorListener = new SensorEventListener()
     {
-
         @Override
         public void onAccuracyChanged(Sensor sensor, int accuracy)
         {
@@ -192,45 +120,19 @@ public class MainActivity extends Activity
     // flash light
     public void turnOnFlashLight()
     {
-
-        try
-        {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
             {
-
                 mTorchOnOffButton.setImageResource(R.drawable.ic_launcher_background);
-
-
-
-
-
-
-
-
             }
-        } catch (Exception e)
-        {
-            e.printStackTrace();
-        }
     }
 
     // flash light
     public void turnOffFlashLight()
     {
-
-        try
-        {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
             {
-
                 mTorchOnOffButton.setImageResource(R.mipmap.ic_launcher);
-                mCameraManager.setTorchMode(mCameraId, false);
             }
-
-        } catch (Exception e)
-        {
-            e.printStackTrace();
-        }
     }
 
 
