3693b47a22f25689b4cd7fa11483559d833e8cba
kevinGRASSET <ke.grasset@laposte.net>
2018-03-14 16:07:39 +0100

    Google Maps

--- app/src/main/java/fr/mmi/rodet/project_android_studio/Page_Parametre.java
+++ app/src/main/java/fr/mmi/rodet/project_android_studio/Page_Parametre.java
@@ -1,6 +1,6 @@
 package fr.mmi.rodet.project_android_studio;
 
-import android.app.Activity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,7 +9,7 @@ import android.view.View;
  * Created by Kevin on 14/03/2018.
  */
 
-public class Page_Parametre extends Activity {
+public class Page_Parametre extends MapActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
@@ -18,6 +18,11 @@ public class Page_Parametre extends Activity {
     public void Page21 (View view)
     {
         startActivity(new Intent(this, MainActivity.class));
+    }
+
+
+    protected boolean isRouteDisplayed() {
+        return false;
     }
 
 }
