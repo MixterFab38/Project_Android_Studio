4570d75e849b8ce341689456dc4967eecc83f880
kevinGRASSET <ke.grasset@laposte.net>
2018-03-14 17:09:49 +0100

    Google Maps fonctionnnelle

--- app/src/main/java/fr/mmi/rodet/project_android_studio/Page_Parametre.java
+++ app/src/main/java/fr/mmi/rodet/project_android_studio/Page_Parametre.java
@@ -3,16 +3,32 @@ package fr.mmi.rodet.project_android_studio;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.View;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * Created by Kevin on 14/03/2018.
  */
 
-public class Page_Parametre extends MapActivity {
+public class Page_Parametre extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
     public void Page21 (View view)
@@ -21,8 +37,12 @@ public class Page_Parametre extends MapActivity {
     }
 
 
-    protected boolean isRouteDisplayed() {
-        return false;
-    }
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
 
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
 }
